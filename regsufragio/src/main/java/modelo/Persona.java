75265d5be248fbddb4fff89ce7520c7dcc88afe0
mkxvls <cielodespejado@protonmail.com>
2021-10-18 13:47:19 -0300

    persona

---
+++ regsufragio/src/main/java/modelo/Persona.java
@@ -0,0 +1,18 @@
+package modelo;
+
+/**
+ * Persona abstracta
+ * @author Maximiliano Valencia Saez
+ */
+public abstract class Persona {
+    private String rut;
+    private String Nombres;
+    private String Apellidos;
+    private String idMesa;
+    public Persona(){
+        
+    }
+    
+    
+    
+}
